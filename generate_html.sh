70e55b5fafcdc9996f5c7bb92accceee46334a20
mercury011 <m.latinis@gmail.com>
2023-02-18 10:55:07 +0100

    Refactor

--- generate_html.sh
+++ generate_html.sh
@@ -1,5 +1,9 @@
-MENU=$(cat menu.html)
+MENU=$(cat menu.misc)
 export MENU
-cat index.html_template | envsubst >index.html
-cat contact.html_template | envsubst >contact.html
+cat index.template | envsubst >index.html
+../styleguide/html/format_html.sh index.html
+cat contact.template | envsubst >contact.html
+../styleguide/html/format_html.sh contact.html
+cat about_us.template | envsubst >about_us.html
+../styleguide/html/format_html.sh about_us.html
 
