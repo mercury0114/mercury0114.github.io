1fd8dcdb9d81f902c20b0495d318e1f6669b8386
mercury011 <m.latinis@gmail.com>
2023-02-18 17:41:44 +0100

    Moving services to a separate tab

--- templates/generate_html.sh
+++ templates/generate_html.sh
@@ -6,5 +6,7 @@ cat contact.html_template | envsubst >contact.html
 ../../styleguide/html/format_html.sh contact.html
 cat about_us.html_template | envsubst >about_us.html
 ../../styleguide/html/format_html.sh about_us.html
+cat services.html_template | envsubst >services.html
+../../styleguide/html/format_html.sh services.html
 mv *.html ../
 
