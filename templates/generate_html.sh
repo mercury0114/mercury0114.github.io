f3a3a2a80afe5d2e442b5b22f2bb07856f5aa4a7
mercury011 <m.latinis@gmail.com>
2023-02-18 13:57:45 +0100

    Moving templates to a separate directory

--- templates/generate_html.sh
+++ templates/generate_html.sh
@@ -1,9 +1,10 @@
 MENU=$(cat menu.misc)
 export MENU
 cat index.html_template | envsubst >index.html
-../styleguide/html/format_html.sh index.html
+../../styleguide/html/format_html.sh index.html
 cat contact.html_template | envsubst >contact.html
-../styleguide/html/format_html.sh contact.html
+../../styleguide/html/format_html.sh contact.html
 cat about_us.html_template | envsubst >about_us.html
-../styleguide/html/format_html.sh about_us.html
+../../styleguide/html/format_html.sh about_us.html
+mv *.html ../
 
