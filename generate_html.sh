ae4a2ef548188f8786126b25235af3e8a1044484
mercury011 <m.latinis@gmail.com>
2023-02-18 11:05:33 +0100

    Renamte .template -> .html_template

--- generate_html.sh
+++ generate_html.sh
@@ -1,9 +1,9 @@
 MENU=$(cat menu.misc)
 export MENU
-cat index.template | envsubst >index.html
+cat index.html_template | envsubst >index.html
 ../styleguide/html/format_html.sh index.html
-cat contact.template | envsubst >contact.html
+cat contact.html_template | envsubst >contact.html
 ../styleguide/html/format_html.sh contact.html
-cat about_us.template | envsubst >about_us.html
+cat about_us.html_template | envsubst >about_us.html
 ../styleguide/html/format_html.sh about_us.html
 
